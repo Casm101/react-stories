58c81f65b7dedf7e9535da1c87c11c324afcc4fe
Christian Smith Mantas <christian.smith-mantas@leovegas.com>
2025-01-01 15:18:30 +0100

    WIP: Happy New Year!!

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { ConfigEnv, defineConfig } from 'vite';
 
-export default defineConfig({
-  base: '/react-stories/',
-  plugins: [react()],
-})
+export default defineConfig((configEnv: ConfigEnv) => {
+  const { mode } = configEnv;
+  const isLibMode = (mode as string) === 'lib';
+
+  if (isLibMode) {
+    console.log('In here - building library');
+    return {
+      plugins: [react()],
+      build: {
+        lib: {
+          entry: 'index.ts',
+          name: 'ReactStories',
+          fileName: 'react-stories'
+        }
+      }
+    };
+  } else {
+    console.log('In here - building site');
+    return {
+      plugins: [react()],
+      base: '/react-stories/',
+      build: {
+        outDir: 'dist'
+      }
+    };
+  }
+});
